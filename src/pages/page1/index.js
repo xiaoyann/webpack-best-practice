1befa7b66e7e7283f75b7934e37dddb053870f38
xiaoyann <0x0886@gmail.com>
2016-07-12 19:48:02 +0800

    init repo

---
+++ src/pages/page1/index.js
@@ -0,0 +1,10 @@
+import React, { component } from 'react';
+import './index.scss';
+
+export default class Page1 extends Component {
+  render() {
+    return (
+      <div className="page1"> Page1 </div>
+    );
+  }
+}

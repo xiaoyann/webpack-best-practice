4deb1564cfbe2039d57382d219a13c945a1c6296
xiaoyann <0x0886@gmail.com>
2016-07-13 20:11:47 +0800

    完成 release

--- build/webpack.release.js
+++ build/webpack.release.js
@@ -1,6 +1,23 @@
 var webpack = require('webpack');
 var config = require('./webpack.config');
 
+var args = process.argv;
+var online = args.indexOf('--deploy=online') > -1;
+var watch = args.indexOf('--watch') > -1;
+
+// 测试环境静态资源 domain
+var testPublicPath = '/';
+// 生产环境静态资源 domain
+var onlinePublicPath = '/';
+
+
+if (online) {
+  config.output.publicPath = onlinePublicPath; 
+} else {
+  config.output.publicPath = testPublicPath; 
+}
+
+
 // 压缩 js, css
 config.plugins.push(
   new webpack.optimize.UglifyJsPlugin({
@@ -9,3 +26,28 @@ config.plugins.push(
     }
   })
 );
+
+
+var compiler = webpack(config);
+
+function callback(err, stats) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(stats.toString({
+      colors: true,
+      chunks: false,
+      children: false,
+    }))  
+  }
+}
+
+if (watch) {
+  compiler.watch({}, callback);
+} else {
+  compiler.run(callback);
+}
+
+
+
+
